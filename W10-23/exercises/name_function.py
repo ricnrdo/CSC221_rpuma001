19f89482bba9b57bf41b022a5d31432840044fad
Ricardo Puma <103985497+ricnrdo@users.noreply.github.com>
2023-10-29 00:06:06 -0400

    Create name_function.py

---
+++ W10-23/exercises/name_function.py
@@ -0,0 +1,20 @@
+# Example one
+# def get_formatted_name(first, last):
+#     """Generate a neatly formatted full name."""
+#     full_name = f"{first} {last}"
+#     return full_name.title()
+
+# Example two
+# def get_formatted_name(first, middle, last):
+#     """Generate a neatly formatted full name."""
+#     full_name = f"{first} {middle} {last}"
+#     return full_name.title()
+
+# Example three
+def get_formatted_name(first, last, middle=''):
+    """Generate a neatly formatted full name."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+    return full_name.title()
